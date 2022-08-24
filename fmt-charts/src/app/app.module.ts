e615d0a9be3ebe2620a753b2b25b12e15e5bcc09
Peter Meyer Elphick Frandsen <petermfrandsen@gmail.com>
2022-08-24 15:56:47 +0200

    Add angular plotly.js to component

--- fmt-charts/src/app/app.module.ts
+++ fmt-charts/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, DoBootstrap, Injector, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { SankeyComponent } from './charts/sankey/sankey.component';
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
     SankeyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PlotlyModule
   ],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
--- fmt-charts/src/app/charts/sankey/sankey.component.ts
+++ fmt-charts/src/app/charts/sankey/sankey.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Plotly } from 'angular-plotly.js/lib/plotly.interface';
 
 @Component({
   selector: 'app-sankey',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sankey.component.css']
 })
 export class SankeyComponent implements OnInit {
+  data = [
+    { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
+    { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+  ];
+  layout = {width: 320, height: 240, title: 'A Fancy Plot'};
 
   constructor() { }
 
